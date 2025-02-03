33c7bbc37f6d12c5c3abc80ff9ff7702d906de9f
Hapikc <136709525532hs@gmail.com>
2025-02-03 23:05:05 +0700

    10 practice task done, but nyans

--- js/main.js
+++ js/main.js
@@ -33,6 +33,19 @@ Vue.component('product', {
             <button v-on:click="deleteToCart">Delete to cart</button>
         </div>
    </div>
+   <div>
+<h2>Reviews</h2>
+<p v-if="!reviews.length">There are no reviews yet.</p>
+<ul>
+  <li v-for="review in reviews">
+  <p>{{ review.name }}</p>
+  <p>Rating: {{ review.rating }}</p>
+  <p>{{ review.review }}</p>
+  </li>
+</ul>
+</div>
+
+   
  `,
     data() {
         return {
@@ -61,7 +74,8 @@ Vue.component('product', {
                     }
                 ],
                 sizes: ['S', 'M', 'L', 'XL', 'XXL', 'XXXL'],
-                cart: []
+                cart: [],
+                reviews: []
         }
     },
     methods: {
@@ -122,6 +136,88 @@ Vue.component('product-details', {
     }
 })
 
+Vue.component('product-review', {
+    template: `
+    <form class="review-form" @submit.prevent="onSubmit">
+         <p v-if="errors.length">
+                <b>Please correct the following error(s):</b>
+                <ul>
+                    <li v-for="error in errors">{{ error }}</li>
+                </ul>
+        </p>
+
+ <p>
+   <label for="name">Name:</label>
+   <input id="name" v-model="name" placeholder="name">
+ </p>
+
+ <p>
+   <label for="review">Review:</label>
+   <textarea id="review" v-model="review"></textarea>
+ </p>
+
+ <p>
+   <label for="rating">Rating:</label>
+   <select id="rating" v-model.number="rating">
+     <option>5</option>
+     <option>4</option>
+     <option>3</option>
+     <option>2</option>
+     <option>1</option>
+   </select>
+  
+ </p>
+ <p>
+           <label class="rec">Can you recommend this staff?</label>
+           <input type="radio" id="rec" name="drone" value="Да" v-model="recommended">
+           <label for="rec">Yes</label>
+           <input type="radio" id="not_rec" name="drone" value="Нет" v-model="recommended">
+           <label for="not_rec">No</label>
+         </p>
+
+ <p>
+   <input type="submit" value="Submit"> 
+   
+ </p>
+
+
+</form>
+
+ `,
+    data() {
+        return {
+            name: null,
+            review: null,
+            rating: null,
+            errors: [],
+            recommended: null
+
+
+        }
+    },
+    methods:{
+        onSubmit() {
+            if(this.name && this.review && this.rating) {
+                let productReview = {
+                    name: this.name,
+                    review: this.review,
+                    rating: this.rating
+                }
+                this.$emit('review-submitted', productReview)
+                this.name = null
+                this.review = null
+                this.rating = null
+            } else {
+                if(!this.name) this.errors.push("Name required.")
+                if(!this.review) this.errors.push("Review required.")
+                if(!this.rating) this.errors.push("Rating required.")
+            }
+        }
+
+    }
+})
+
+
 
 let app = new Vue({
     el: '#app',
@@ -135,6 +231,9 @@ let app = new Vue({
         },
         removeCart(id) {
             this.cart.pop(id);
+        },
+        addReview(productReview) {
+            this.reviews.push(productReview)
         }
     }
 
